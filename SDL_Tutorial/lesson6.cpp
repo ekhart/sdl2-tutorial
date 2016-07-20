eca96e04c2550727f02c9c7adc95231231e48451
Artur Dębkowski <arturdebkowski@gmail.com>
2016-07-20 21:05:40 +0200

    Using renderText

--- SDL_Tutorial/lesson6.cpp
+++ SDL_Tutorial/lesson6.cpp
@@ -49,76 +49,28 @@ int main(int, char**) {
 		return 3;
 	}
 
-	// Loading a Bitmap Image
-	string resPath = getResourcePath("Lesson5");
-	auto *background = loadTexture(resPath + "background.png", renderer);
-	auto *image = loadTexture(resPath + "image.png", renderer);
-	if (!background || !image) {
-		cleanup(background, image, renderer, window);
-		logSDLError("loding textures");
-		IMG_Quit();
+	string resPath = getResourcePath("Lesson6");
+	// We'll render the string "TTF font are cool!" in white
+	// Color is in RGBA format
+	SDL_Color color = { 255, 255, 255, 255 };
+	SDL_Texture *image = renderText("TTF fonts are cool!", resPath + "sample.ttf", color, 64, renderer);
+	if (image == nullptr) {
+		cleanup(renderer, window);
+		TTF_Quit();
 		SDL_Quit();
 		return 4;
 	}
 
 	//iW and iH are the clip width and height
 	//We'll be drawing only clips so get a center position for the w/h of a clip
-	int iW = 100, iH = 100,
-		x = center(SCREEN_WIDTH, iW),
+	int iW, iH;
+	SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
+	
+	int x = center(SCREEN_WIDTH, iW),
 		y = center(SCREEN_HEIGHT, iH);
 
-	//Setup the clips for our image
-	const int CLIPS_COUNT = 4;
-	SDL_Rect clips[CLIPS_COUNT];
-
-	//Since our clips our uniform in size we can generate a list of their
-	//positions using some math (the specifics of this are coverd in the lesson)
-	for (int i = 0; i < CLIPS_COUNT; ++i) {
-		auto clip = &clips[i];
-		clip->x = half(i) * iW;
-		clip->y = i % 2 * iH;
-		clip->w = iW;
-		clip->h = iH;
-	}
-
-	//Specify a default clip to start with 
-	int useClip = 0;
-
-	// Completing the Main Loop
-	// our event structure
-	SDL_Event event;
-	auto quit = false;
-	while (!quit) {
-		while (SDL_PollEvent(&event)) {
-			quit = event.type == SDL_QUIT;
-
-			//Use number input to select which clip should be drawn
-			if (event.type == SDL_KEYDOWN) {
-				switch (event.key.keysym.sym)
-				{
-				case SDLK_1: useClip = 0; break;
-				case SDLK_2: useClip = 1; break;
-				case SDLK_3: useClip = 2; break;
-				case SDLK_4: useClip = 3; break;
-				case SDLK_ESCAPE: quit = true; break;
-				default:
-					break;
-				}
-			}
-		}
-
-		// render the scene
-		SDL_RenderClear(renderer);
-
-		//Drawing the image
-		renderTexture(image, renderer, x, y, &clips[useClip]);
-
-		//Update the screen
-		SDL_RenderPresent(renderer);
-	}
-
 	// Cleaning Up
-	cleanup(background, image, renderer, window);
+	cleanup(image, renderer, window);
 	IMG_Quit();
 	SDL_Quit();
 
